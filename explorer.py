75d6ff75f8dfeb96a9398611e5c253f97427aecd
sanada8 <sarath.kumar@beldex.io>
2021-05-26 13:52:48 +0530

    initial flask and lmq connection for connecting beldex masternode

--- explorer.py
+++ explorer.py
@@ -6,6 +6,10 @@ import sys
 import statistics
 import string
 import requests
+from werkzeug.routing import BaseConverter
+from lmq import FutureJSON, lmq_connection
+
+app = flask.Flask(__name__)
 
 class Hex64Converter(BaseConverter):
     def __init__(self, url_map):
@@ -13,3 +17,13 @@ class Hex64Converter(BaseConverter):
         self.regex = "[0-9a-fA-F]{64}"
 
 app.url_map.converters['hex64'] = Hex64Converter
+
+
+@app.route('/page/<int:page>')
+@app.route('/page/<int:page>/<int:per_page>')
+@app.route('/range/<int:first>/<int:last>')
+@app.route('/autorefresh/<int:refresh>')
+@app.route('/')
+
+def main(refresh=None, page=0, per_page=None, first=None, last=None):
+    lmq, beldexd = lmq_connection()
